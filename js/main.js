d72e7864467968fd81af61f30d888df1ac3c98d6
Francesco <francescomartinmail@gmail.com>
2020-11-10 18:46:26 +0100

    first toDoList

--- js/main.js
+++ js/main.js
@@ -5,19 +5,28 @@ const app = new Vue({
   data: {
     toDo: '',
     time: '',
+    task: '',
     newToDo: [],
     newTime: [],
 
   },
   methods: {
-    saveElement: function() {
+    saveElement() {
 
-      // this.newToDo.push(this.toDo);
+      this.newToDo.push(this.toDo);
+      this.toDo = null;
+      this.task = this.newToDo.length;
       //
       // if (newToDo.includes(this.toDo)) {
       //   this.newTime.push(this.time);
       // }
 
-    }
+    },
+    removeToDo(index) {
+
+      this.newToDo.splice(index, 1);
+      this.task = this.newToDo.length;
+    },
+
   }
 }); // ==> FINISH VUE READY
